bb309ef215f57ec66660a6d70257b5af45973d1c
jevvonn <darmawanb985@gmail.com>
2025-04-26 10:46:59 +0700

    feat: add User entity with GORM model definition

---
+++ internal/domain/entity/user.go
@@ -0,0 +1,17 @@
+package entity
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type User struct {
+	ID       uuid.UUID `gorm:"primaryKey" json:"id,omitempty"`
+	Name     string    `gorm:"type:varchar(255);not null" json:"name,omitempty"`
+	Email    string    `gorm:"type:varchar(255);index;not null;unique" json:"email,omitempty"`
+	Password string    `gorm:"type:varchar(255);not null;"`
+
+	CreatedAt time.Time `json:"created_at,omitempty"`
+	UpdatedAt time.Time `json:"updated_at,omitempty"`
+}

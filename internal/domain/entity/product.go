1afbb9bf5c67a8ec4ea02fea21a3efd64dcaf5fe
Jevon <112501785+jevvonn@users.noreply.github.com>
2025-04-26 22:35:34 +0700

    feat/products to main (#4)
    * feat: implement product management with RESTful API and PostgreSQL repository
    
    * feat: enhance product entity and usecase with cloth type and motif ID handling
    
    * fix: add missing DB_HOST environment variable in docker-compose

--- internal/domain/entity/product.go
+++ internal/domain/entity/product.go
@@ -7,9 +7,11 @@ import (
 )
 
 type Product struct {
-	ID       uuid.UUID `gorm:"primaryKey" json:"id,omitempty"`
-	Name     string    `gorm:"type:text;not null" json:"name,omitempty"`
-	ImageURL string    `gorm:"type:varchar(255);not null" json:"image_url,omitempty"`
+	ID        uuid.UUID `gorm:"primaryKey" json:"id,omitempty"`
+	Name      string    `gorm:"type:text;not null" json:"name,omitempty"`
+	ImageURL  string    `gorm:"type:varchar(255);not null" json:"image_url,omitempty"`
+	ClothType string    `gorm:"type:varchar(50);not null" json:"cloth_type,omitempty"`
+	MotifID   uuid.UUID `gorm:"type:uuid;not null" json:"motif_id,omitempty"`
 
 	Sizes []ProductSizeVariant `gorm:"foreignKey:ProductID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"sizes,omitempty"`
 

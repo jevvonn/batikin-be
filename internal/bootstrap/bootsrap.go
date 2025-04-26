5073c63ba6a06d4efd1020b4d161225ca437c5c8
jevvonn <darmawanb985@gmail.com>
2025-04-26 10:42:31 +0700

    feat: implement bootstrap package to initialize Fiber app and PostgreSQL connection

---
+++ internal/bootstrap/bootsrap.go
@@ -0,0 +1,33 @@
+package bootstrap
+
+import (
+	"batikin-be/config"
+	"batikin-be/internal/infra/postgresql"
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func Start() error {
+	app := fiber.New()
+	conf := config.New()
+
+	dsn := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable TimeZone=Asia/Jakarta",
+		conf.DbHost,
+		conf.DbPort,
+		conf.DbUser,
+		conf.DbPassword,
+		conf.DbName,
+	)
+	_, err := postgresql.New(dsn)
+	if err != nil {
+		panic(err)
+	}
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Your API is running")
+	})
+
+	return app.Listen(fmt.Sprintf(":%s", conf.AppPort))
+}
